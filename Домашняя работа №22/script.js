719f507954db48232ae06aca6ff915c927ba2bdf
Valerka <76082412+valerka03@users.noreply.github.com>
2025-03-24 10:14:18 +0600

    Add files via upload

---
+++ Домашняя работа №22/script.js
@@ -0,0 +1,16 @@
+// вам необходимо через массив arr вывести строку metalabs
+
+let a = [1, { test2: { true: false }, test: 21 }, { meta: ['metalabs', 113] }]
+
+let arr = [
+	'color',
+	[
+		1222,
+		{
+			meta: ['labs', 117673],
+			meta: ['labs', 110, { finally: 'check', arr2: ['hello world', a] }],
+		},
+	],
+]
+
+console.log(arr[1][1].meta[2].arr2[1][2].meta[0])
